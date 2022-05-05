ad6edbffe3b098d367e704ad2245ca2cf3ecbeec
azizovmukhammad <azizovmuhammaduz@gmail.com>
2022-05-05 13:56:07 +0500

    make it

--- src/main/java/com/example/registrationsystem/config/SwaggerConfig.java
+++ src/main/java/com/example/registrationsystem/config/SwaggerConfig.java
@@ -11,6 +11,7 @@ import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -42,7 +43,7 @@ public class SwaggerConfig {
         AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
         AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
         authorizationScopes[0] = authorizationScope;
-        return List.of(new SecurityReference("JWT", authorizationScopes));
+        return Collections.singletonList(new SecurityReference("JWT", authorizationScopes));
     }
 
     private ApiInfo metadata() {
